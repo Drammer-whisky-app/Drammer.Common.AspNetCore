8efde7ab0e52334297301df67ae375b7c5496457
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-10-18 16:29:48 +0200

    ✨ Added ToHtml5DateTimeString

---
+++ src/Drammer.Common.AspNetCore.Tests/SchemaOrg/DateTimeExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Drammer.Common.AspNetCore.SchemaOrg;
+
+namespace Drammer.Common.AspNetCore.Tests.SchemaOrg;
+
+public sealed class DateTimeExtensionsTests
+{
+    [Fact]
+    public void ToHtml5DateTimeString_WhenDateTimeIsDefault_ReturnsEmptyString()
+    {
+        // Arrange
+        var dateTime = default(DateTime);
+
+        // Act
+        var result = dateTime.ToHtml5DateTimeString();
+
+        // Assert
+        Assert.Equal(string.Empty, result);
+    }
+
+    [Fact]
+    public void ToHtml5DateTimeString_WhenDateTimeIsNotDefault_ReturnsFormattedString()
+    {
+        // Arrange
+        var dateTime = new DateTime(2021, 1, 1, 12, 0, 0);
+
+        // Act
+        var result = dateTime.ToHtml5DateTimeString();
+
+        // Assert
+        Assert.Equal("2021-01-01T12:00:00", result);
+    }
+}
---
+++ src/Drammer.Common.AspNetCore/SchemaOrg/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Drammer.Common.AspNetCore.SchemaOrg;
+
+public static class DateTimeExtensions
+{
+    /// <summary>
+    ///     Returns a HTML5 datetime string.
+    /// </summary>
+    /// <param name="dateTime">
+    ///     The date time.
+    /// </param>
+    /// <returns>
+    ///     The <see cref="string" />.
+    /// </returns>
+    public static string ToHtml5DateTimeString(this DateTime dateTime)
+    {
+        return dateTime == default ? string.Empty : dateTime.ToString("yyyy-MM-ddTHH:mm:ss");
+    }
+}
