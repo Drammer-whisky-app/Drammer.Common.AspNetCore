9672f28cb6d7ca4939569514b58c417cbf121f12
Marthijn van den Heuvel <marthijn@sidio.nl>
2025-09-05 16:42:25 +0200

    ✨ Add ViewContextExtensions with query parameter utilities

---
+++ src/Drammer.Common.AspNetCore.Tests/Mvc/ViewContextExtensionsTests.cs
@@ -0,0 +1,103 @@
+﻿using Drammer.Common.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Drammer.Common.AspNetCore.Tests.Mvc;
+
+public sealed class ViewContextExtensionsTests
+{
+    [Fact]
+    public void GetAllQueryParameters_ReturnsDictionary()
+    {
+        // Arrange
+        var httpContext = new DefaultHttpContext
+        {
+            Request =
+            {
+                QueryString = new QueryString("?key1=value1&key2=value2")
+            }
+        };
+
+        var viewContext = new ViewContext
+        {
+            HttpContext = httpContext,
+        };
+
+        // Act
+        var result = viewContext.GetAllQueryParameters();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value2" },
+        });
+    }
+
+    [Fact]
+    public void WithValue_AddsValue()
+    {
+        // Arrange
+        var dict = new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value2" },
+        };
+
+        // Act
+        var result = dict.WithValue("key3", "value3");
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value2" },
+            { "key3", "value3" },
+        });
+    }
+
+    [Fact]
+    public void WithValue_UpdatesValue()
+    {
+        // Arrange
+        var dict = new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value2" },
+        };
+
+        // Act
+        var result = dict.WithValue("key2", "value3");
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value3" },
+        });
+    }
+
+    [Fact]
+    public void WithValue_RemovesValue()
+    {
+        // Arrange
+        var dict = new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value2" },
+        };
+
+        // Act
+        var result = dict.WithValue("key2", null);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+        });
+    }
+}
---
+++ src/Drammer.Common.AspNetCore/Mvc/ViewContextExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Drammer.Common.AspNetCore.Mvc;
+
+/// <summary>
+/// The view context extensions.
+/// </summary>
+public static class ViewContextExtensions
+{
+    /// <summary>
+    /// Gets all query parameters from the current request.
+    /// </summary>
+    /// <param name="viewContext">The view context.</param>
+    /// <returns>A dictionary.</returns>
+    public static Dictionary<string, string> GetAllQueryParameters(this ViewContext viewContext)
+    {
+        var query = viewContext.HttpContext.Request.Query;
+        return query.ToDictionary(x => x.Key, x => x.Value.ToString());
+    }
+
+    /// <summary>
+    /// Sets or removes a value in a dictionary, returning a new dictionary.
+    /// </summary>
+    /// <param name="dict">The dictionary.</param>
+    /// <param name="key">The key.</param>
+    /// <param name="value">The value.</param>
+    /// <returns>A dictionary.</returns>
+    public static Dictionary<string, string> WithValue(this Dictionary<string, string> dict, string key, string? value)
+    {
+        var newDict = new Dictionary<string, string>(dict);
+        if (value == null)
+        {
+            newDict.Remove(key);
+        }
+        else
+        {
+            newDict[key] = value;
+        }
+
+        return newDict;
+    }
+}
